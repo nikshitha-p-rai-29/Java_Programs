ea32d66a1a56a1fe56f4b0097c5828fd40af37ab
Nikshitha P Rai <nikshithaprai2004@gmail.com>
2025-05-23 18:19:11 +0530

    added successfully

--- Lab1_ArrayListandLinkedList/Lab1c_LinkedList/LinkedListIterate.java
+++ Lab1_ArrayListandLinkedList/Lab1c_LinkedList/LinkedListIterate.java
@@ -1,5 +1,5 @@
 //1. Write a Java program to iterate through all elements in a linked list starting at the  specified position ( 2nd) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1)) 
-package ArrayListandLinkedList.Lab1c_LinkedList;
+package Lab1_ArrayListandLinkedList.Lab1c_LinkedList;
 import java.util.*;
 public class LinkedListIterate {
     public static void main(String[] args) {
